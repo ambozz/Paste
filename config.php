27003dab5b1e928ec80f067bc25a32d577a1443d
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 01:38:23 +0200

    fix error: Undefined constant due to missing quotation marks

--- config.php
+++ config.php
@@ -8,15 +8,15 @@ define('DB_NAME','database');
 // Language Config
 
 // paste.php
-define(PASTE_P_NOT_DEFINED,"Error p not defined.");
+define('PASTE_P_NOT_DEFINED',"Error p not defined.");
 
 // create.php
-define(CREATE_CONTENT_NOT_DEFINED,"Error content not defined.");
-define(CREATE_INSERT_FAILED,"Oops! Something went wrong. Please try again later.");
+define('CREATE_CONTENT_NOT_DEFINED',"Error content not defined.");
+define('CREATE_INSERT_FAILED',"Oops! Something went wrong. Please try again later.");
 
 // install.php
-define(INSTALL_PASTES_TABLE_CREATED,"Table 'pastes' was created successfully.");
-define(INSTALL_PRIMARY_KEY_ID_ADDED,"Primary Key 'id' added successfully.");
+define('INSTALL_PASTES_TABLE_CREATED',"Table 'pastes' was created successfully.");
+define('INSTALL_PRIMARY_KEY_ID_ADDED',"Primary Key 'id' added successfully.");
 
 // Attempt to connect to MySQL database
 $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
