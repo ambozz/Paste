8c3d1398ebe1a02d9dda8f84d0b349444d27552f
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 01:26:19 +0200

    create paste functionality in create.php

--- create.php
+++ create.php
@@ -2,5 +2,28 @@
 // Import config.php
 require_once("config.php");
 
+if(isset($_POST["content"])){
+    // Get ID for paste
+    $id = uniqid();
+
+    $sql = "INSERT INTO `pastes`(`id`, `content`) VALUES (?,?)";
+            
+    if($stmt = mysqli_prepare($mysqli, $sql)){
+        mysqli_stmt_bind_param($stmt, "ss", $param_id, $param_content);
+        
+        $param_id = $id;
+        $param_content = $_POST["content"];
+        
+        if(mysqli_stmt_execute($stmt)){
+            header("Location: paste.php?p={$id}");
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+}else{
+    exit("Error content not defined.");
+}
 
 ?>
