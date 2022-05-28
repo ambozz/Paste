b74bc5260d54c28421213c32fac2b48f17a24a53
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 01:32:17 +0200

    added language config to config.php

--- config.php
+++ config.php
@@ -5,6 +5,19 @@ define('DB_PASSWORD','password');
 define('DB_SERVER','localhost');
 define('DB_NAME','database');
 
+// Language Config
+
+// paste.php
+define(PASTE_P_NOT_DEFINED,'database');
+
+// create.php
+define(CREATE_CONTENT_NOT_DEFINED,'database');
+define(CREATE_INSERT_FAILED,'database');
+
+// install.php
+define(INSTALL_PASTES_TABLE_CREATED,'database');
+define(INSTALL_PRIMARY_KEY_ID_ADDED,'database');
+
 // Attempt to connect to MySQL database
 $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
