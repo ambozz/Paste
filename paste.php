8fe219de636c0f375b03eed2564fdf5f3ee08c3e
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 01:27:50 +0200

    add html structure from index.php to paste.php

---
+++ paste.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pastehub</title>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="header">
+            <h1>Pastehub</h1>
+        </div>
+        <div class="wrapper">
+            <form>
+                <textarea name="content" readonly></textarea>
+            </form>
+        </div>
+        <div class="footer">
+            <h3>Made with ❤️ by Pastehub</h3>
+        </div>
+    </body>
+</html>
