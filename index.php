3256b671b0d182d48f1c41f19a5e9ed809b9c6bf
ambozz <amboss1337@protonmail.com>
2022-05-29 00:02:58 +0200

    add index.php with base structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pastehub</title>
+</head>
+<body>
+    <div class="header"></div>
+    <div class="wrapper">
+        <form method="POST" action="create.php">
+            <textarea name="content">
+            <button type="submit">Paste</button>
+        </form>
+    </div>
+    <div class="footer"></div>
+</body>
+</html>
