8d5deb9ae738f8ef496e6972f7d5dca7d9124165
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 01:12:03 +0200

    create table pastes in install.php

--- install.php
+++ install.php
@@ -2,4 +2,9 @@
 // Import config.php
 require_once("config.php");
 
+// Create pastes table
+if($result = $mysqli -> query("CREATE TABLE `pastes` (`id` varchar(16) NOT NULL,`content` longtext NOT NULL,`create_time` timestamp NOT NULL DEFAULT current_timestamp());")){
+    echo("Table 'pastes' was created successfully.");
+}
+
 ?>
