ee43d4f5c96e028b736f1f54cfa2b245d3a8df6d
ambozz <silkenbeumerfynn@gmail.com>
2022-05-29 00:41:40 +0200

    structured html better

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pastehub</title>
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    <div class="header">
-        <h1>Pastehub</h1>
-    </div>
-    <div class="wrapper">
-        <form method="POST" action="create.php">
-            <textarea name="content"></textarea>
-            <button type="submit">Paste</button>
-        </form>
-    </div>
-    <div class="footer">
-        <h3>Made with ❤️ by Pastehub</h3>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pastehub</title>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="header">
+            <h1>Pastehub</h1>
+        </div>
+        <div class="wrapper">
+            <form method="POST" action="create.php">
+                <textarea name="content"></textarea>
+                <button type="submit">Paste</button>
+            </form>
+        </div>
+        <div class="footer">
+            <h3>Made with ❤️ by Pastehub</h3>
+        </div>
+    </body>
 </html>
