f5e1abf74612ca3f1012bbe3e0fb3bf398c2dc53
ambozz <74627962+ambozz@users.noreply.github.com>
2023-06-24 20:29:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pastehub</title>
+        <title>Paste</title>
         <link href="assets/css/style.css" rel="stylesheet" type="text/css">
         <link href="assets/css/responsive.css" rel="stylesheet" type="text/css">
 
@@ -18,7 +18,7 @@
     <body>
         <div class="darkmode_button" onclick="switchdarkmode();"></div>
         <div class="header">
-            <a href="/"><h1>Pastehub</h1></a>
+            <a href="/"><h1>Paste</h1></a>
         </div>
         <div class="wrapper">
             <form method="POST" action="create.php">
@@ -27,7 +27,7 @@
             </form>
         </div>
         <div class="footer">
-            <h3>Made with &#10084;&#65039; by Pastehub</h3>
+            <h3>Made with &#10084;&#65039; by Paste</h3>
         </div>
     </body>
 </html>
